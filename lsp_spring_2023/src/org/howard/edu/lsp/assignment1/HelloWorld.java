063f07fb12eafd60f822578790f7fed91b70ff5a
c-admas18 <61594318+c-admas18@users.noreply.github.com>
2023-01-25 19:37:38 -0500

    This is my first commit to Github.

---
+++ lsp_spring_2023/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Name: Chrystell Adams
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
